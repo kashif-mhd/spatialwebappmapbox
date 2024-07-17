a1219a0892a38b402303ec44405f0b0b9f659eb6
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2024-07-17 23:40:13 +0500

    Fix browser router defined at 2 places.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthProvider';
-import Login from './components/auth/Login';
-import Signup from './components/auth/Signup';
-import ForgotPassword from './components/auth/ForgotPassword';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import { AuthProvider } from "./context/AuthProvider";
 import Dashboard from "./pages/dashboard/Dashboard";
-import './App.css';
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword/>} />
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </AuthProvider>
   );
 };
