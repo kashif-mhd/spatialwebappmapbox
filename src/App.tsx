006ff21f05b7c9cae4dd5ac6e1725f051284acec
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2024-07-22 23:07:28 +0500

    Add secure routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,60 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Login from "./components/auth/login";
-import Signup from "./components/auth/signup";
 import ForgotPassword from "./components/auth/forgotPassword";
+import Login from "./components/auth/login";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import PublicRoute from "./components/auth/PublicRoute";
+import Signup from "./components/auth/signup";
 import { AuthProvider } from "./context/authProvider";
+import Dashboard from "./pages/dashboard/Dashboard";
 import Details from "./pages/detail/Details";
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/detail" element={<Details />} />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute restricted={true}>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute restricted={true}>
+              <Signup />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/forgot-password"
+          element={
+            <PublicRoute restricted={true}>
+              <ForgotPassword />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/detail"
+          element={
+            <ProtectedRoute>
+              <Details />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </AuthProvider>
   );
