cde6e05263e766e89b27b11c24dedeeaae95daf1
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2024-07-28 03:06:26 +0500

    fix tables UI

--- src/pages/detail/Details.tsx
+++ src/pages/detail/Details.tsx
@@ -7,6 +7,7 @@ import { Input } from '../../components/ui/input'
 import { DatePicker } from '../../components/DatePicker'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../components/ui/tabs"
 import { CstTable } from './table/CstTable'
+import { ColumnDef } from '@tanstack/react-table'
 
 const frameworks = [
   {
@@ -31,8 +32,176 @@ const frameworks = [
   },
 ]
 
-const Details = () => {
+type PastoralData = {
+  id: string;
+  landClass: string;
+  area: number;
+  valuePerHa: number;
+  totalValue: number;
+  msPerHa: number;
+  totalMs: number;
+  suPerHa: number;
+  totalSu: number;
+  description: string;
+};
 
+type ImprovementData = {
+  id: string;
+  improvement: string;
+  unit: number;
+  ratePerUnit: number;
+  value: number;
+  condition: string;
+  description: string;
+};
+
+type InclusiontData = {
+  id: string;
+  improvement: string;
+  unit: number;
+  ratePerUnit: number;
+  value: number;
+  description: string;
+};
+type DetailChattelsData = {
+  id: string;
+  item: string;
+  unit: number;
+  ratePerUnit: number;
+  value: number;
+  description: string;
+};
+
+const pastoralData: PastoralData[] = [
+  {
+    id: '1',
+    landClass: 'Class A',
+    area: 100,
+    valuePerHa: 5000,
+    totalValue: 500000,
+    msPerHa: 50,
+    totalMs: 5000,
+    suPerHa: 10,
+    totalSu: 1000,
+    description: 'Description A',
+  },
+  {
+    id: '2',
+    landClass: 'Class B',
+    area: 200,
+    valuePerHa: 6000,
+    totalValue: 1200000,
+    msPerHa: 60,
+    totalMs: 12000,
+    suPerHa: 20,
+    totalSu: 4000,
+    description: 'Description B',
+  },
+];
+
+
+const improvementData: ImprovementData[] = [
+  {
+    id: '1',
+    improvement: 'Fence',
+    unit: 10,
+    ratePerUnit: 100,
+    value: 1000,
+    condition: 'Good',
+    description: 'Wooden fence around property',
+  },
+  {
+    id: '2',
+    improvement: 'Barn',
+    unit: 1,
+    ratePerUnit: 5000,
+    value: 5000,
+    condition: 'Excellent',
+    description: 'Large barn for storage',
+  },
+];
+
+
+
+const inclusionData: InclusiontData[] = [
+  {
+    id: '1',
+    improvement: 'Fence',
+    unit: 10,
+    ratePerUnit: 100,
+    value: 1000,
+    description: 'Wooden fence around property',
+  },
+  {
+    id: '2',
+    improvement: 'Barn',
+    unit: 1,
+    ratePerUnit: 5000,
+    value: 5000,
+    description: 'Large barn for storage',
+  },
+];
+
+const detailChattelsData: DetailChattelsData[] = [
+  {
+    id: '1',
+    item: 'Furniture',
+    unit: 5,
+    ratePerUnit: 200,
+    value: 1000,
+    description: 'Living room furniture',
+  },
+  {
+    id: '2',
+    item: 'Appliances',
+    unit: 3,
+    ratePerUnit: 500,
+    value: 1500,
+    description: 'Kitchen appliances',
+  },
+];
+
+
+
+
+const pastoralColumns: ColumnDef<PastoralData>[] = [
+  { accessorKey: 'landClass', header: 'Land Class' },
+  { accessorKey: 'area', header: 'Area (Ha)' },
+  { accessorKey: 'valuePerHa', header: 'Value (Ha)' },
+  { accessorKey: 'totalValue', header: 'Total Value' },
+  { accessorKey: 'msPerHa', header: 'MS/Ha' },
+  { accessorKey: 'totalMs', header: 'Total MS' },
+  { accessorKey: 'suPerHa', header: 'SU/Ha' },
+  { accessorKey: 'totalSu', header: 'Total SU' },
+  { accessorKey: 'description', header: 'Description' },
+];
+const improvementColumns: ColumnDef<ImprovementData>[] = [
+  { accessorKey: 'improvement', header: 'Improvement' },
+  { accessorKey: 'unit', header: 'Unit' },
+  { accessorKey: 'ratePerUnit', header: 'Rate /Unit' },
+  { accessorKey: 'value', header: 'Value' },
+  { accessorKey: 'condition', header: 'Cond' },
+  { accessorKey: 'description', header: 'Description' },
+];
+
+
+
+const inclusionColumns: ColumnDef<InclusiontData>[] = [
+  { accessorKey: 'improvement', header: 'Item' },
+  { accessorKey: 'unit', header: 'Unit' },
+  { accessorKey: 'ratePerUnit', header: 'Rate /Unit' },
+  { accessorKey: 'value', header: 'Value' },
+  { accessorKey: 'description', header: 'Description' },
+];
+const detailChattelsColumns: ColumnDef<DetailChattelsData>[] = [
+  { accessorKey: 'item', header: 'Item' },
+  { accessorKey: 'unit', header: 'Unit' },
+  { accessorKey: 'ratePerUnit', header: 'Rate /Unit' },
+  { accessorKey: 'value', header: 'Value' },
+  { accessorKey: 'description', header: 'Description' },
+];
+
+const Details = () => {
   return (
     <Container>
       <div className='px-6'>
@@ -43,51 +212,51 @@ const Details = () => {
           <div className="grid gap-4">
             <Card className='p-3'>
               <div className="grid grid-cols-4 gap-3">
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Vender/Purchaser</Label>
-                  <div >
+                  <div>
                     <Combobox frameworks={frameworks} />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Street No:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Address:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Property Name:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Distance:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" value={"17km N of Tokoroa "} disabled />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Sale Date:</Label>
-                  <div >
+                  <div>
                     <DatePicker />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>RT:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Use:</Label>
-                  <div >
+                  <div>
                     <Combobox frameworks={frameworks} />
                   </div>
                 </div>
@@ -96,185 +265,198 @@ const Details = () => {
 
             <Card className='p-3'>
               <div className="grid grid-cols-4 gap-3">
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Relationship:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Total Area:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Roll:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>GCV:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
- <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Analysed By:</Label>
-                  <div >
+                  <div>
                     <Combobox frameworks={frameworks} />
                   </div>
                 </div>
-           
               </div>
-          
             </Card>
+
             <Card className='p-3'>
-              
               <div className="grid grid-cols-3 gap-3">
-                     <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Tenure:</Label>
-                  <div >
+                  <div>
                     <Combobox frameworks={frameworks} />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>GLV:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Analyses Status:</Label>
-                  <div >
+                  <div>
                     <Combobox frameworks={frameworks} />
                   </div>
                 </div>
-
               </div>
             </Card>
+
             <Card className='p-3'>
-              
               <div className="grid grid-cols-4 gap-3 ">
-                     <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Gross Sale Price:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Ineff Area:</Label>
-                  <div >
+                  <div>
                     <Input type="text" placeholder="" />
                   </div>
                 </div>
-                <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Date:</Label>
-                  <div >
-                    <DatePicker/>
+                  <div>
+                    <DatePicker />
                   </div>
                 </div>
-                  <div >
+                <div>
                   <Label htmlFor="" className='block mb-2'>Date Analysed:</Label>
-                  <div >
-                    <DatePicker/>
+                  <div>
+                    <DatePicker />
                   </div>
                 </div>
-
               </div>
             </Card>
+
             <div className="grid grid-cols-2 gap-4">
-               <Card className='p-3'>
+              <Card className='p-3'>
                 <div className="grid grid-cols-2 gap-4">
-                   <div >
-                  <Label htmlFor="" className='block mb-2'>Gross Sale Price:</Label>
-                  <div >
-                    <Input type="text" placeholder="" />
+                  <div>
+                    <Label htmlFor="" className='block mb-2'>Gross Sale Price:</Label>
+                    <div>
+                      <Input type="text" placeholder="" />
+                    </div>
+                  </div>
+                  <div>
+                    <Label htmlFor="" className='block mb-2'>Ineff Area:</Label>
+                    <div>
+                      <Input type="text" placeholder="" />
+                    </div>
+                  </div>
+                  <div>
+                    <Label htmlFor="" className='block mb-2'>Date:</Label>
+                    <div>
+                      <DatePicker />
+                    </div>
+                  </div>
+                  <div>
+                    <Label htmlFor="" className='block mb-2'>Date Analysed:</Label>
+                    <div>
+                      <DatePicker />
+                    </div>
                   </div>
                 </div>
-                <div >
-                  <Label htmlFor="" className='block mb-2'>Ineff Area:</Label>
-                  <div >
-                    <Input type="text" placeholder="" />
-                  </div>
-                </div>
-                <div >
-                  <Label htmlFor="" className='block mb-2'>Date:</Label>
-                  <div >
-                    <DatePicker/>
-                  </div>
-                </div>
-                  <div >
-                  <Label htmlFor="" className='block mb-2'>Date Analysed:</Label>
-                  <div >
-                    <DatePicker/>
-                  </div>
-                </div>
-                </div>
-               </Card>
-                <Card className='p-3'>
+              </Card>
+              <Card className='p-3'>
                 <div className="grid grid-cols-2 gap-4">
-                   <div >
-                  <Label htmlFor="" className='block mb-2'>Gross Sale Price:</Label>
-                  <div >
-                    <Input type="text" placeholder="" />
+                  <div>
+                    <Label htmlFor="" className='block mb-2'>Gross Sale Price:</Label>
+                    <div>
+                      <Input type="text" placeholder="" />
+                    </div>
+                  </div>
+                  <div>
+                    <Label htmlFor="" className='block mb-2'>Ineff Area:</Label>
+                    <div>
+                      <Input type="text" placeholder="" />
+                    </div>
+                  </div>
+                  <div>
+                    <Label htmlFor="" className='block mb-2'>Date:</Label>
+                    <div>
+                      <DatePicker />
+                    </div>
+                  </div>
+                  <div>
+                    <Label htmlFor="" className='block mb-2'>Date Analysed:</Label>
+                    <div>
+                      <DatePicker />
+                    </div>
                   </div>
                 </div>
-                <div >
-                  <Label htmlFor="" className='block mb-2'>Ineff Area:</Label>
-                  <div >
-                    <Input type="text" placeholder="" />
-                  </div>
-                </div>
-                <div >
-                  <Label htmlFor="" className='block mb-2'>Date:</Label>
-                  <div >
-                    <DatePicker/>
-                  </div>
-                </div>
-                  <div >
-                  <Label htmlFor="" className='block mb-2'>Date Analysed:</Label>
-                  <div >
-                    <DatePicker/>
-                  </div>
-                </div>
-                </div>
-               </Card>
+              </Card>
             </div>
+
             <Tabs defaultValue="account" className="w-full">
-            <TabsList>
-              <TabsTrigger value="account">Notes</TabsTrigger>
-              <TabsTrigger value="password">Sales Description</TabsTrigger>
-            </TabsList>
-            <TabsContent value="account">
-              <Card className='p-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit voluptatum sapiente error sunt aut exercitationem enim similique, est cumque dolore voluptas sed iste autem a et soluta fugiat maxime odit?
-              </Card>
+              <TabsList>
+                <TabsTrigger value="account">Notes</TabsTrigger>
+                <TabsTrigger value="password">Sales Description</TabsTrigger>
+              </TabsList>
+              <TabsContent value="account">
+                <Card className='p-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit voluptatum sapiente error sunt aut exercitationem enim similique, est cumque dolore voluptas sed iste autem a et soluta fugiat maxime odit?</Card>
               </TabsContent>
-            <TabsContent value="password">
-              <Card className='p-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit voluptatum sapiente error sunt aut exercitationem enim similique, est cumque dolore voluptas sed iste autem a et soluta fugiat maxime odit?
-              </Card>
-            </TabsContent>
-          </Tabs>
-          <div className="grid grid-cols-2 gap-4">
-             <Card>
+              <TabsContent value="password">
+                <Card className='p-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit voluptatum sapiente error sunt aut exercitationem enim similique, est cumque dolore voluptas sed iste autem a et soluta fugiat maxime odit?</Card>
+              </TabsContent>
+            </Tabs>
+
+            <div className="grid grid-cols-2 gap-4">
+              <Card>
                 <CardHeader className='pb-2'>
                   <h4 className='font-semibold'>Pastoral Land</h4>
                 </CardHeader>
                 <CardContent className='pt-0'>
-                  <CstTable/>
+            
+                <CstTable columns={pastoralColumns} data={pastoralData} />
                 </CardContent>
-             </Card>
-                <Card>
+              </Card>
+              <Card>
                 <CardHeader className='pb-2'>
                   <h4 className='font-semibold'>Improvements</h4>
                 </CardHeader>
                 <CardContent className='pt-0'>
-                  <CstTable/>
+                  <CstTable data={improvementData} columns={improvementColumns} />
                 </CardContent>
-             </Card>
-            
-             </div>
+              </Card>
+               <Card>
+                <CardHeader className='pb-2'>
+                  <h4 className='font-semibold'>Inclusion</h4>
+                </CardHeader>
+                <CardContent className='pt-0'>
+      <CstTable data={inclusionData} columns={inclusionColumns} />
+                </CardContent>
+              </Card>
+                  <Card>
+                <CardHeader className='pb-2'>
+                  <h4 className='font-semibold'>Detail Chattels</h4>
+                </CardHeader>
+                <CardContent className='pt-0'>
+      <CstTable data={detailChattelsData} columns={detailChattelsColumns} />
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/detail/table/CstTable.tsx
+++ src/pages/detail/table/CstTable.tsx
@@ -15,9 +15,7 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-
 import { Button } from "../../../components/ui/button";
-import { Checkbox } from "../../../components/ui/checkbox";
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -33,121 +31,16 @@ import {
   TableHeader,
   TableRow,
 } from "../../../components/ui/table";
-import DropdownMenuCst from "../../../components/DropdownMenuCst";
 import SheetMain from "../../../components/SheetMain";
 
-const data: Payment[] = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    email: "ken99@yahoo.com",
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    email: "Abe45@gmail.com",
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    email: "Monserrat44@gmail.com",
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    email: "Silas22@gmail.com",
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    email: "carmella@hotmail.com",
-  },
-];
-
-export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
+type CstTableProps<T> = {
+  data: T[];
+  columns: ColumnDef<T>[];
 };
 
-export function CstTable() {
+export function CstTable<T>({ data, columns }: CstTableProps<T>) {
   const [isSheetOpen, setIsSheetOpen] = React.useState(false);
-
-  const columns: ColumnDef<Payment>[] = [
-    {
-      id: "select",
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && "indeterminate")
-          }
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-        />
-      ),
-      enableSorting: false,
-      enableHiding: false,
-    },
-    {
-      accessorKey: "status",
-      header: "Status",
-      cell: ({ row }) => <div className="capitalize">{row.getValue("status")}</div>,
-    },
-    {
-      accessorKey: "email",
-      header: ({ column }) => (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      ),
-      cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
-    },
-    {
-      accessorKey: "amount",
-      header: () => <div className="text-right">Amount</div>,
-      cell: ({ row }) => {
-        const amount = parseFloat(row.getValue("amount"));
-
-        // Format the amount as a dollar amount
-        const formatted = new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "USD",
-        }).format(amount);
-
-        return <div className="text-right font-medium">{formatted}</div>;
-      },
-    },
-    {
-      id: "actions",
-      enableHiding: false,
-      cell: ({ row }) => {
-        return (
-          <DropdownMenuCst
-            setIsSheetOpen={setIsSheetOpen}
-            row={row}
-          />
-        );
-      },
-    },
-  ];
+  const [globalFilter, setGlobalFilter] = React.useState("");
 
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
@@ -156,32 +49,52 @@ export function CstTable() {
 
   const table = useReactTable({
     data,
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
+    columns: [
+      {
+        id: "select",
+        header: ({ table }) => (
+          <input
+            type="checkbox"
+            checked={table.getIsAllRowsSelected()}
+            onChange={table.getToggleAllRowsSelectedHandler()}
+          />
+        ),
+        cell: ({ row }) => (
+          <input
+            type="checkbox"
+            checked={row.getIsSelected()}
+            onChange={row.getToggleSelectedHandler()}
+          />
+        ),
+      },
+      ...columns,
+    ],
     state: {
       sorting,
       columnFilters,
       columnVisibility,
       rowSelection,
+      globalFilter,
     },
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    onGlobalFilterChange: setGlobalFilter,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    globalFilterFn: "includesString",
   });
 
   return (
     <div className="w-full">
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
-          }
+          placeholder="Filter..."
+          value={globalFilter}
+          onChange={(event) => setGlobalFilter(event.target.value)}
           className="max-w-sm"
         />
         <DropdownMenu>
@@ -235,7 +148,7 @@ export function CstTable() {
               ))
             ) : (
               <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
+                <TableCell colSpan={columns.length + 1} className="h-24 text-center">
                   No results.
                 </TableCell>
               </TableRow>
