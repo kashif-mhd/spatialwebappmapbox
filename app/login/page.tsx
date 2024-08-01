95a5482aad8b1e555bba103ea9c2082d1ea1fe19
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2024-08-02 00:20:16 +0500

    Remove back arrow.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -54,26 +54,6 @@ export default function Login({ searchParams }: { searchParams: { message: strin
   return (
     <OnBoarding>
       <div className="flex-1 flex flex-col px-8 sm:max-w-md justify-center gap-2 w-full">
-        <Link
-          href="/"
-          className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-          >
-            <polyline points="15 18 9 12 15 6" />
-          </svg>{' '}
-          Back
-        </Link>
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <div className="text-center">
             <img src="./images/logo.svg" alt="" className="h-12 mb-8 inline-block" />
