42c0792a2df5e8acc557daee887eeac127df0eaf
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2024-08-15 21:45:51 +0500

    Add api for fetching localities.

--- app/dairy-form/page.tsx
+++ app/dairy-form/page.tsx
@@ -4,16 +4,32 @@ import { createClient } from '@/utils/supabase/server'
 import Container from '@/components/Container'
 import DataForm from './components/DataForm'
 
+export type LocalityData = {
+  id: string,
+  locality: string,
+  region: string,
+  district: string
+}
+
 export default async function DairyForm() {
-  // const supabase = createClient()
+  const supabase = createClient()
 
-  // const {
-  //   data: { user }
-  // } = await supabase.auth.getUser()
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
 
-  // if (!user) {
-  //   return redirect('/login')
-  // }
+  if (!user) {
+    return redirect('/login')
+  }
+
+  const { data: localityData, error } = await supabase
+  .from('localities')
+  .select('*')
+
+if (error) {
+  console.error('Error fetching locality data:', error)
+  return <div>Error loading data</div>
+}
 
   return (
     <Container>
@@ -22,7 +38,7 @@ export default async function DairyForm() {
           <span className="font-semibold text-xl">Dashboard</span>
         </div>
 
-        <DataForm />
+        <DataForm localityData={localityData}/>
       </div>
     </Container>
   )
