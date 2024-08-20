f5a751ae3328dfd76c97d6b4bcdfdfb38ff0ee1c
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2024-08-20 14:53:13 +0500

    Add improvement types options from supabase.

--- app/dairy-form/components/tables/ImprovementsTable.tsx
+++ app/dairy-form/components/tables/ImprovementsTable.tsx
@@ -1,104 +1,136 @@
-'use client'
+"use client";
 
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
-import { FieldsType, improvementColumns } from '../formData'
-import { TableFields } from './TableFields'
-import { FormField } from './DrawerForm'
-import { FieldArray, useFormikContext } from 'formik'
-import * as yup from 'yup'
-import { useMemo } from 'react'
-
-const formFields: FormField[] = [
-  {
-    label: 'Improvement',
-    name: 'improvement',
-    type: 'text',
-    groupClassName: 'grid grid-cols-4 items-center gap-4',
-    labelClassName: 'text-right',
-    className: 'col-span-3'
-  },
-  {
-    label: 'Unit',
-    name: 'unit',
-    type: 'number',
-    groupClassName: 'grid grid-cols-4 items-center gap-4',
-    labelClassName: 'text-right',
-    className: 'col-span-3'
-  },
-  {
-    label: 'Rate',
-    name: 'rate',
-    type: 'number',
-    groupClassName: 'grid grid-cols-4 items-center gap-4',
-    labelClassName: 'text-right',
-    className: 'col-span-3'
-  },
-  {
-    label: 'Value',
-    name: 'value',
-    type: 'number',
-    groupClassName: 'grid grid-cols-4 items-center gap-4',
-    labelClassName: 'text-right',
-    className: 'col-span-3',
-    readOnly: true
-  },
-  {
-    label: 'Condition',
-    name: 'condition',
-    type: 'select',
-    options: [
-      {
-        label: 'Excellent',
-        value: 'excellent'
-      },
-      {
-        label: 'Good',
-        value: 'good'
-      },
-      {
-        label: 'Fair',
-        value: 'fair'
-      },
-      {
-        label: 'Poor',
-        value: 'poor'
-      }
-    ],
-    groupClassName: 'grid grid-cols-4 items-center gap-4',
-    labelClassName: 'text-right',
-    className: 'col-span-3'
-  },
-  {
-    label: 'Description',
-    name: 'description',
-    type: 'textarea',
-    groupClassName: 'grid gap-4'
-  }
-]
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { createClient } from "@/utils/supabase/client";
+import { FieldArray, useFormikContext } from "formik";
+import { useEffect, useMemo, useState } from "react";
+import * as yup from "yup";
+import { FieldsType, improvementColumns } from "../formData";
+import { FormField } from "./DrawerForm";
+import { TableFields } from "./TableFields";
 
 const improvementValidationSchema = yup.object().shape({
-  improvement: yup.string().required().label('Improvement'),
-  unit: yup.number().required().label('Unit'),
-  rate: yup.number().required().label('Rate'),
-  value: yup.number().required().label('Value'),
-  condition: yup.string().required().label('Condition'),
-  description: yup.string().required().label('Description')
-})
+  improvement: yup.string().required().label("Improvement"),
+  unit: yup.number().required().label("Unit"),
+  rate: yup.number().required().label("Rate"),
+  value: yup.number().required().label("Value"),
+  condition: yup.string().required().label("Condition"),
+  description: yup.string().required().label("Description"),
+});
 
 export const ImprovementsTable = () => {
-  const formik = useFormikContext<FieldsType>()
+  const formik = useFormikContext<FieldsType>();
+  const [improvementTypeOptions, setImprovementTypesOptions] = useState<
+    { label: string; value: string }[]
+  >([]);
+
+  const formFields: FormField[] = [
+    {
+      label: "Improvement",
+      name: "improvement",
+      type: "select",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+      options: improvementTypeOptions,
+    },
+    {
+      label: "Unit",
+      name: "unit",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "Rate",
+      name: "rate",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "Value",
+      name: "value",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+      readOnly: true,
+    },
+    {
+      label: "Condition",
+      name: "condition",
+      type: "select",
+      options: [
+        {
+          label: "Excellent",
+          value: "excellent",
+        },
+        {
+          label: "Good",
+          value: "good",
+        },
+        {
+          label: "Fair",
+          value: "fair",
+        },
+        {
+          label: "Poor",
+          value: "poor",
+        },
+      ],
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "Description",
+      name: "description",
+      type: "textarea",
+      groupClassName: "grid gap-4",
+    },
+  ];
+
   const footerData = useMemo(() => {
-    const data = formik.values.improvements || []
+    const data = formik.values.improvements || [];
     return {
-      id: '',
-      improvement: 'Total',
+      id: "",
+      improvement: "Total",
       unit: 0,
       ratePerUnit: 0,
       value: data.reduce((acc, row) => acc + row.value, 0),
-      condition: '',
-      description: ''
-    }
-  }, [formik.values.improvements])
+      condition: "",
+      description: "",
+    };
+  }, [formik.values.improvements]);
+
+  useEffect(() => {
+    const fetchImprovementTypes = async () => {
+      const supabase = createClient();
+      const { data, error } = await supabase
+        .from("list_improvements")
+        .select("id, strimprovementtype");
+
+      if (error) {
+        console.error("Error fetching improvment types:", error.message);
+        return;
+      }
+
+      if (data) {
+        const options = data.map(
+          (item: { id: string; strimprovementtype: string }) => ({
+            label: item.strimprovementtype,
+            value: item.id,
+          })
+        );
+        setImprovementTypesOptions(options);
+      }
+    };
+
+    fetchImprovementTypes();
+  }, []);
 
   return (
     <Card>
@@ -115,14 +147,14 @@ export const ImprovementsTable = () => {
               formFieldsValidationSchema={improvementValidationSchema}
               footerData={footerData}
               onAdd={(row) => {
-                const calculatedRow = { ...row }
-                calculatedRow.value = row.unit * row.ratePerUnit
-                arrayHelpers.push(row)
+                const calculatedRow = { ...row };
+                calculatedRow.value = row.unit * row.ratePerUnit;
+                arrayHelpers.push(row);
               }}
               onEdit={(index, row) => {
-                const calculatedRow = { ...row }
-                calculatedRow.value = row.unit * row.ratePerUnit
-                arrayHelpers.replace(index, row)
+                const calculatedRow = { ...row };
+                calculatedRow.value = row.unit * row.ratePerUnit;
+                arrayHelpers.replace(index, row);
               }}
               onDelete={(index) => arrayHelpers.remove(index)}
             />
@@ -130,7 +162,7 @@ export const ImprovementsTable = () => {
         </FieldArray>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default ImprovementsTable
+export default ImprovementsTable;
--- app/dairy-form/components/tables/PastoralLandTable.tsx
+++ app/dairy-form/components/tables/PastoralLandTable.tsx
@@ -1,37 +1,121 @@
-'use client'
+"use client";
 
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
-import { FC, useEffect, useMemo, useState } from 'react'
-import { FieldsType, pastoralColumns } from '../formData'
-import { TableFields } from './TableFields'
-import { FormField } from './DrawerForm'
-import { FieldArray, useFormikContext } from 'formik'
-import * as yup from 'yup'
-import { createClient } from '@/utils/supabase/client'
-
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { createClient } from "@/utils/supabase/client";
+import { FieldArray, useFormikContext } from "formik";
+import { FC, useEffect, useMemo, useState } from "react";
+import * as yup from "yup";
+import { FieldsType, pastoralColumns } from "../formData";
+import { FormField } from "./DrawerForm";
+import { TableFields } from "./TableFields";
 
 const pastoralLandValidationSchema = yup.object().shape({
-  landClass: yup.string().required().label('Land Class'),
-  area: yup.number().required().label('Area (Ha)'),
-  valuePerHa: yup.number().required().label('Value/Ha'),
-  totalValue: yup.number().required().label('Total Value'),
-  msPerHa: yup.number().required().label('MS/Ha'),
-  totalMs: yup.number().required().label('Total MS'),
-  suPerHa: yup.number().required().label('SU/Ha'),
-  totalSu: yup.number().required().label('Total SU'),
-  inOff: yup.boolean().nullable().label('In Off'),
-  description: yup.string().required().label('Description')
-})
+  landClass: yup.string().required().label("Land Class"),
+  area: yup.number().required().label("Area (Ha)"),
+  valuePerHa: yup.number().required().label("Value/Ha"),
+  totalValue: yup.number().required().label("Total Value"),
+  msPerHa: yup.number().required().label("MS/Ha"),
+  totalMs: yup.number().required().label("Total MS"),
+  suPerHa: yup.number().required().label("SU/Ha"),
+  totalSu: yup.number().required().label("Total SU"),
+  inOff: yup.boolean().nullable().label("In Off"),
+  description: yup.string().required().label("Description"),
+});
 
 export const PastoralLandTable: FC = () => {
-  const formik = useFormikContext<FieldsType>()
-  const [landClassOptions, setLandClassOptions] = useState<{ label: string; value: string }[]>([]);
-  
+  const formik = useFormikContext<FieldsType>();
+  const [landClassOptions, setLandClassOptions] = useState<
+    { label: string; value: string }[]
+  >([]);
+
+  const formFields: FormField[] = [
+    {
+      label: "Land Class",
+      name: "landClass",
+      type: "select",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+      options: landClassOptions,
+    },
+    {
+      label: "Area (Ha)",
+      name: "area",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "Value/Ha",
+      name: "valuePerHa",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "Value",
+      name: "totalValue",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "MS/Ha",
+      name: "msPerHa",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "Total MS",
+      name: "totalMs",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "SU/Ha",
+      name: "suPerHa",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "Total SU",
+      name: "totalSu",
+      type: "number",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+      readOnly: true,
+    },
+    {
+      type: "checkbox",
+      label: "In Off",
+      name: "inOff",
+      groupClassName: "grid grid-cols-4 items-center gap-4",
+      labelClassName: "text-right",
+      className: "col-span-3",
+    },
+    {
+      label: "Description",
+      name: "description",
+      type: "textarea",
+      groupClassName: "grid gap-4",
+    },
+  ];
+
   const footerData = useMemo(() => {
-    const data = formik.values.pastoral_land || []
+    const data = formik.values.pastoral_land || [];
     return {
-      id: '',
-      landClass: 'Total',
+      id: "",
+      landClass: "Total",
       area: data.reduce((acc, row) => acc + row.area, 0),
       valuePerHa: data.reduce((acc, row) => acc + row.valuePerHa, 0), // Replace with calculation if needed
       totalValue: data.reduce((acc, row) => acc + row.totalValue, 0),
@@ -40,108 +124,29 @@ export const PastoralLandTable: FC = () => {
       suPerHa: 0, // Replace with calculation if needed
       totalSu: data.reduce((acc, row) => acc + row.totalSu, 0),
       inOff: false,
-      description: ''
-    }
-  }, [formik.values.pastoral_land])
-
-  const formFields: FormField[] = [
-    {
-      type: 'select',
-      label: 'Land Class',
-      name: 'landClass',
-      groupClassName: 'grid grid-cols-4 items-center gap-4',
-      labelClassName: 'text-right',
-      className: 'col-span-3',
-      options: landClassOptions,
-    },
-    {
-      label: 'Area (Ha)',
-      name: 'area',
-      type: 'number',
-      groupClassName: 'grid grid-cols-4 items-center gap-4',
-      labelClassName: 'text-right',
-      className: 'col-span-3'
-    },
-    {
-      label: 'Value/Ha',
-      name: 'valuePerHa',
-      type: 'number',
-      groupClassName: 'grid grid-cols-4 items-center gap-4',
-      labelClassName: 'text-right',
-      className: 'col-span-3'
-    },
-    {
-      label: 'Value',
-      name: 'totalValue',
-      type: 'number',
-      groupClassName: 'grid grid-cols-4 items-center gap-4',
-      labelClassName: 'text-right',
-      className: 'col-span-3'
-    },
-    {
-      label: 'MS/Ha',
-      name: 'msPerHa',
-      type: 'number',
-      groupClassName: 'grid grid-cols-4 items-center gap-4',
-      labelClassName: 'text-right',
-      className: 'col-span-3'
-    },
-    {
-      label: 'Total MS',
-      name: 'totalMs',
-      type: 'number',
-      groupClassName: 'grid grid-cols-4 items-center gap-4',
-      labelClassName: 'text-right',
-      className: 'col-span-3'
-    },
-    {
-      label: 'SU/Ha',
-      name: 'suPerHa',
-      type: 'number',
-      groupClassName: 'grid grid-cols-4 items-center gap-4',
-      labelClassName: 'text-right',
-      className: 'col-span-3'
-    },
-    {
-      label: 'Total SU',
-      name: 'totalSu',
-      type: 'number',
-      groupClassName: 'grid grid-cols-4 items-center gap-4',
-      labelClassName: 'text-right',
-      className: 'col-span-3',
-      readOnly: true
-    },
-    {
-      type: 'checkbox',
-      label: 'In Off',
-      name: 'inOff',
-      groupClassName: 'grid grid-cols-4 items-center gap-4',
-      labelClassName: 'text-right',
-      className: 'col-span-3'
-    },
-    {
-      label: 'Description',
-      name: 'description',
-      type: 'textarea',
-      groupClassName: 'grid gap-4'
-    }
-  ]
+      description: "",
+    };
+  }, [formik.values.pastoral_land]);
 
   useEffect(() => {
     const fetchLandClasses = async () => {
       const supabase = createClient();
-      const { data, error } = await supabase.from('list_pastoral_land_classes').select('id, strlandclass');
+      const { data, error } = await supabase
+        .from("list_pastoral_land_classes")
+        .select("id, strlandclass");
 
       if (error) {
-        console.error('Error fetching land classes:', error.message);
+        console.error("Error fetching land classes:", error.message);
         return;
       }
 
       if (data) {
-        const options = data.map((item: { id: string; strlandclass: string }) => ({
-          label: item.strlandclass,
-          value: item.id,
-        }));
+        const options = data.map(
+          (item: { id: string; strlandclass: string }) => ({
+            label: item.strlandclass,
+            value: item.id,
+          })
+        );
         setLandClassOptions(options);
       }
     };
@@ -149,7 +154,6 @@ export const PastoralLandTable: FC = () => {
     fetchLandClasses();
   }, []);
 
-  
   return (
     <Card>
       <CardHeader className="pb-2">
@@ -165,65 +169,84 @@ export const PastoralLandTable: FC = () => {
               formFieldsValidationSchema={pastoralLandValidationSchema}
               footerData={footerData}
               onAdd={(row) => {
-                const calculatedRow = { ...row }
-                calculatedRow.totalSu = row.suPerHa * row.area
-                arrayHelpers.push(calculatedRow)
+                const calculatedRow = { ...row };
+                calculatedRow.totalSu = row.suPerHa * row.area;
+                arrayHelpers.push(calculatedRow);
               }}
               onEdit={(index, row) => {
-                const calculatedRow = { ...row }
-                calculatedRow.totalSu = row.suPerHa * row.area
-                arrayHelpers.replace(index, row)
+                const calculatedRow = { ...row };
+                calculatedRow.totalSu = row.suPerHa * row.area;
+                arrayHelpers.replace(index, row);
               }}
               onDelete={(index) => arrayHelpers.remove(index)}
               bulkAction={{
-                label: 'Calculate',
+                label: "Calculate",
                 onClick: (rows) => {
                   for (const row of rows) {
-                    const index = row.index
+                    const index = row.index;
 
                     const area =
                       formik.values.total_area -
                       formik.values.pastoral_land
                         .filter((value, index) => index !== index)
-                        .reduce((acc, row) => acc + row.area, 0)
-                    const valuePerHa = formik.values.pastoral_land[index].totalValue / area
+                        .reduce((acc, row) => acc + row.area, 0);
+                    const valuePerHa =
+                      formik.values.pastoral_land[index].totalValue / area;
                     const totalValue =
                       formik.values.land_value -
                       formik.values.site_total_value -
                       formik.values.pastoral_land
                         .filter((value, index) => index !== index)
-                        .reduce((acc, row) => acc + row.totalValue, 0)
+                        .reduce((acc, row) => acc + row.totalValue, 0);
 
                     const totalMs =
                       formik.values.avg_eff_kgms -
                       formik.values.pastoral_land
                         .filter((value, index) => index !== index)
-                        .reduce((acc, row) => acc + row.totalMs, 0)
-                    const msPerHa = totalMs / valuePerHa
+                        .reduce((acc, row) => acc + row.totalMs, 0);
+                    const msPerHa = totalMs / valuePerHa;
 
                     const totalSu =
                       formik.values.su -
                       formik.values.pastoral_land
                         .filter((value, index) => index !== index)
-                        .reduce((acc, row) => acc + row.totalSu, 0)
-                    const suPerHa = totalSu / valuePerHa
+                        .reduce((acc, row) => acc + row.totalSu, 0);
+                    const suPerHa = totalSu / valuePerHa;
 
-                    formik.setFieldValue(`pastoral_land[${index}].area`, area)
-                    formik.setFieldValue(`pastoral_land[${index}].valuePerHa`, valuePerHa)
-                    formik.setFieldValue(`pastoral_land[${index}].totalValue`, totalValue)
-                    formik.setFieldValue(`pastoral_land[${index}].msPerHa`, msPerHa)
-                    formik.setFieldValue(`pastoral_land[${index}].totalMs`, totalMs)
-                    formik.setFieldValue(`pastoral_land[${index}].suPerHa`, suPerHa)
-                    formik.setFieldValue(`pastoral_land[${index}].totalSu`, totalSu)
+                    formik.setFieldValue(`pastoral_land[${index}].area`, area);
+                    formik.setFieldValue(
+                      `pastoral_land[${index}].valuePerHa`,
+                      valuePerHa
+                    );
+                    formik.setFieldValue(
+                      `pastoral_land[${index}].totalValue`,
+                      totalValue
+                    );
+                    formik.setFieldValue(
+                      `pastoral_land[${index}].msPerHa`,
+                      msPerHa
+                    );
+                    formik.setFieldValue(
+                      `pastoral_land[${index}].totalMs`,
+                      totalMs
+                    );
+                    formik.setFieldValue(
+                      `pastoral_land[${index}].suPerHa`,
+                      suPerHa
+                    );
+                    formik.setFieldValue(
+                      `pastoral_land[${index}].totalSu`,
+                      totalSu
+                    );
                   }
-                }
+                },
               }}
             />
           )}
         </FieldArray>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default PastoralLandTable
+export default PastoralLandTable;
