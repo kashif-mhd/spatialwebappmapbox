046e3329229653ef68ecec76bf8a8267ea9f25ba
KashifMehmoodLHR <kashifmehmood@ymail.com>
2024-09-05 08:26:30 +0400

    added maps part with functionality completed till now

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -5,3 +5,9 @@ export const createClient = () =>
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
+
+export const createTestClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL_TEST!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_TEST!,
+  );
